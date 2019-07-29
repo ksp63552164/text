2a8ca3a3703c60652b0416e271dd8b44c35f9677
lc <63552164@qq.com>
2019-07-29 15:04:38 +0800

    第si次

--- src/com/my/text.java
+++ src/com/my/text.java
@@ -4,6 +4,7 @@ public class text {
     public static void main(String[] args) {
         System.out.println("hello wored");
         System.out.println("怎么回事GitHub没跟新？？");
+        System.out.println("nima");
 
     }
 }
