e30ec36cc0d7b736a27448a657a277952d5e256e
lc <63552164@qq.com>
2019-07-29 15:02:46 +0800

    第三次

--- src/com/my/text.java
+++ src/com/my/text.java
@@ -3,6 +3,7 @@ package com.my;
 public class text {
     public static void main(String[] args) {
         System.out.println("hello wored");
+        System.out.println("怎么回事GitHub没跟新？？");
 
     }
 }
